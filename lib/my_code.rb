ee72621ffd27486116f949c6d1ddbf2b795c6a7d
Ben Dunjay <bendunjay@outlook.com>
2020-02-21 14:31:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+require 'pry'
+
+def map(array) 
+  new_array = []
+  counter = 0 
+  
+  while counter < array.length do 
+new_array.push(yield array[counter])
+
+
+counter += 1 
+end
+new_array
+end
+
+
+def reduce(array, starting_value = 0)
+  counter = 1 
+starting_point = array[0] 
+
+while counter < array.length do 
+  
+ starting_point = yield(array[counter], starting_point)  
+
+counter += 1
+end 
+starting_point
+  
+end
